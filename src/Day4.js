386f203782bb5f0c9f54b518c276d504f5ddcd0c
haleemah-yousef <haleemahadoos@gmail.com>
2025-10-24 13:50:52 +0300

    solved using dp

--- src/Day4.js
+++ src/Day4.js
@@ -32,5 +32,40 @@ function coinCombination(nump) {
 
 }
 
-console.log(coinCombination(200))
+//console.log(coinCombination(200))
+
+
+
+
+// Using Dynamic Programming DP the memoization approach
+
+
+function countMemo(coins, n, sum, memo) {
+
+
+    if (sum === 0) return 1;
+
+    if (sum<0)
+        return 0;
+    if (n==0)
+        return 0;
+
+    
+    if (memo[n - 1][sum] == -1) 
+    {
+        memo[n - 1][sum] = countMemo(coins, n, sum - coins[n - 1], memo) + countMemo(coins, n - 1, sum, memo);
+    }
+    
+    return memo[n - 1][sum];
+}
+
+function coinCombinationMemo(num)
+{
+    coins = [1,2,5,10,20,50,100,200];
+    let n = coins.length
+    const memo = Array.from({ length: n }, () => Array(num + 1).fill(-1));
+    return countMemo(coins, coins.length, num, memo);
+}
+
+console.log(coinCombinationMemo(200))
 
